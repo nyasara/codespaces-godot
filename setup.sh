da6276543017c19c89fc4c5fbea463a67c8e9c77
nyasara <nyasara@hotmail.com>
2025-06-26 10:50:24 +0800

    Update setup.sh to install Godot 4.4.1

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ echo "tint2 & ~/godot-engine" > ~/.config/openbox/autostart
 git clone https://github.com/novnc/novnc
 sudo apt update
 sudo apt install -y openbox tigervnc-standalone-server tint2 firefox wget pcmanfm termit xdg-utils
-wget "https://github.com/godotengine/godot/releases/download/4.3-stable/Godot_v4.3-stable_linux.x86_64.zip" --output-document="godot.zip"
+wget "https://github.com/godotengine/godot/releases/download/4.4.1-stable/Godot_v4.4.1-stable_linux.x86_64.zip" --output-document="godot.zip"
 unzip -p "godot.zip" > godot-engine
 rm "godot.zip"
 mv godot-engine ~/godot-engine
